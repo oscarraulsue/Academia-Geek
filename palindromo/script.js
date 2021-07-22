76d90cb76caa0c246cfdff9d3c1047f809d8cde7
Oscar Sue <87151266+oscarraulsue@users.noreply.github.com>
2021-07-22 11:52:08 -0500

    Ejercicios Git

--- palindromo/script.js
+++ palindromo/script.js
@@ -6,9 +6,9 @@ function palindromo(texto) {
   alert("el campo esta en vacio")
 }
 else{
-    const x = texto.toLowerCase(); //cambio el texto a minusculas
-    const a = x.replace(/\s/g, ''); //quito los espacios
-    const c = a.split("").reverse().join(""); //invierto el array y cambiar a una palabra
+    const x = texto.toLowerCase(); 
+    const a = x.replace(/\s/g, ''); 
+    const c = a.split("").reverse().join(""); 
   console.log(texto)
     if (a === c) {
       document.getElementById('cont3').innerHTML='la palabra ingresada es un palindromo';
